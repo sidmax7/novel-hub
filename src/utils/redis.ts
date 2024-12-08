608c46398b3b9ae8ca85480e844812961a41117c
Raihaan <52083697+sidmax7@users.noreply.github.com>
2024-12-08 22:17:34 +0530

    create redis.ts

---
+++ src/utils/redis.ts
@@ -0,0 +1,14 @@
+import { Redis } from '@upstash/redis';
+
+// Create Redis instance only on the server side
+const getRedisClient = () => {
+  if (typeof window === 'undefined') {
+    return new Redis({
+      url: process.env.NEXT_PUBLIC_REDIS_URL || '',
+      token: process.env.NEXT_PUBLIC_REDIS_TOKEN || '',
+    });
+  }
+  return null;
+};
+
+export const redis = getRedisClient(); 
